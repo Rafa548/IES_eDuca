b57e72d98652b2d8a2acbba8f26ea6121d864f50
miguel <miguelcruzeiro70@gmail.com>
2023-11-17 15:58:04 +0000

    created project

---
+++ eDuca/src/main/java/ies/g26/eDuca/EDucaApplication.java
@@ -0,0 +1,13 @@
+package ies.g26.eDuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDucaApplication.class, args);
+	}
+
+}
---
+++ eDuca/src/test/java/ies/g26/eDuca/EDucaApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.g26.eDuca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
