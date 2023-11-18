d82930a675a493ee916d585c155e5c5377ebf8f8
DiogoSilva1904 <diogompsilva2000@gmail.com>
2023-11-18 00:13:38 +0000

    models done

---
+++ eDuca_services/src/main/java/ies/g26/eDuca/model/UserType.java
@@ -0,0 +1,5 @@
+package ies.g26.eDuca.model;
+
+public enum UserType {
+        Sys_Admin, Sch_Admin, Teacher, Student
+}
--- eDuca_services/src/main/java/ies/g26/eDuca/model/u_type.java
+++
@@ -1,5 +0,0 @@
-package ies.g26.eDuca.model;
-
-public enum u_type {
-        Sys_Admin, Sch_Admin, Teacher, Student
-}
