641028a359b7083843510b5e06add79268f68862
miguel <miguelcruzeiro70@gmail.com>
2023-11-25 21:08:32 +0000

    kafka communication

--- eDuca_dataGen/GenerateData.py
+++ eDuca_dataGen/GenerateData.py
@@ -1,4 +1,4 @@
-from kafka import KafkaProducer, KafkaConsumer
+""" from kafka import KafkaProducer, KafkaConsumer
 from kafka.errors import KafkaError
 import json
 import time
@@ -40,12 +40,7 @@ def main():
 
     consumer.subscribe([receive_topic])
 
-    """ while True:
-        print("Waiting for messages...")
-        for message in consumer:
-            print(message.value)
-            producer.send(send_topic, message.value)
-     """
+    
     # Initialize the API endpoint
     students_api_url = "http://localhost:8080/students"
     class_api_url = "http://localhost:8080/classes"
@@ -115,6 +110,42 @@ def main():
     
 
     
+            
+
+
+if __name__ == '__main__':
+    main() """
+
+from kafka import KafkaProducer, KafkaConsumer
+from kafka.errors import KafkaError
+import json
+import time
+import random
+
+student_json = '{"nmec":107360,"email":"@gmail.com","name":"diogo silva","password":"hello","school":"andiacityschool"}'
+student_json1 = '{"nmec":1073670,"email":"m@gmail.com","name":"miguel","password":"hello","school":"andiacityschool"}'
+student = json.loads(student_json1)
+
+def main():
+
+    
+    consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group', value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+    
+    send_topic = 'eDuca_dataGen'
+    receive_topic = 'eDucaApp'
+
+    consumer.subscribe([receive_topic])
+
+    while True:
+        print("Waiting for messages...")
+        for message in consumer:
+            print(message.value)
+            #producer.send(send_topic, message.value)
+            if message.value['type'] == 'init':
+                
+                producer.send(send_topic, {"type":"student","name":student['name'],"nmec":student['nmec'],"email":student['email'],"password":student['password'],"school":student['school']})
             
 
 
